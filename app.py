486f94b7bc0f8eae388cac64aae730cf1b54eeaa
xiaoyao <1065645460@qq.com>
2022-04-14 21:28:38 +0800

    修改post接口

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from crypt import methods
 from flask import Flask
+from sqlalchemy import true
 
 # flask run --host=0.0.0.0    接收外网请求
 
@@ -10,10 +12,15 @@ def hello_world():
   return "<p>Hello, World!</p>"
 
 
-@app.route("/get")
-def test():
+@app.route("/api/get")
+def tesGet():
   return "get"
 
 
-if __name__ == '__main__':
-  app.run()
+@app.route("/api/post", methods=['POST', 'GET'])
+def testPost():
+  return "post"
+
+
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=8002, debug=true)
